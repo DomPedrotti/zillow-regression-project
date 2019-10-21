82f551c91897fbd1732bcb4f042f0a27447d4c96
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-10-20 20:43:43 -0500

    modifications

--- modeling.py
+++ modeling.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.linear_model import LinearRegression, LassoCV
+from math import sqrt
+import warnings
+warnings.filterwarnings("ignore")
+
+import split_scale as ss
+import evaluate as ev
+import features as fe
+
 def plot_residuals(x,y, dataframe = None):
     '''
     plot_residuals(x,y, dataframe = None)
@@ -13,3 +29,18 @@ def plot_residuals(x,y, dataframe = None):
     returns None
     '''
     sns.residplot(x,y,data = dataframe)
+
+
+def plot_regression(x, y):
+    sns.relplot(x,y)
+    pass
+
+def model_predictions(x_train, y_train, x_test, y_test, model_type = LinearRegression()):
+    #fit model_object
+    model_object = model_type.fit(x_train, y_train)
+    
+    yhat = model_object.predict(x_test)
+    
+    results = x_test.join(y_test)
+    results['predicted'] = yhat
+    return results
